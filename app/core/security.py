398928a6255a3e00741d94ee56b9bf00bc5c93c4
Aster Ginete <asterginete@yahoo.com>
2023-08-26 22:49:07 +0900

    add authorization and authentication

---
+++ app/core/security.py
@@ -0,0 +1,31 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+import jwt
+from typing import Optional
+
+# For hashing the password
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# For creating and verifying JWT tokens
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"  # Change this!
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token: str):
+    return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
